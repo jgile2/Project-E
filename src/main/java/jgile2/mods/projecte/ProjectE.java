53f37fc7f55f85ad83ea1646c933b99f9560067b
jgile2 <jgile2@optusnet.com.au>
2014-03-15 19:22:01 +1000

    First Commit

---
+++ src/main/java/jgile2/mods/projecte/ProjectE.java
@@ -0,0 +1,31 @@
+package jgile2.mods.projecte;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(name = "Project E", version = "0.0.1", useMetadata = false, modid = "ProjectE")
+public class ProjectE {
+
+	@Instance("ProjectE")
+	public static ProjectE instance;
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+	@EventHandler 
+	public void init(FMLInitializationEvent event){
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event){
+		
+	}
+
+}
