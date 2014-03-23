71e0b2649f044ff1a7c34772ef26e0082f9ddc41
jgile2 <jgile2@optusnet.com.au>
2014-03-23 10:21:23 +1000

    Some new stuff

--- src/main/java/projecte/blocks/RelayMK1.java
+++ src/main/java/projecte/blocks/RelayMK1.java
@@ -1,12 +1,16 @@
 package projecte.blocks;
 
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
@@ -19,6 +23,8 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class RelayMK1 extends BlockContainer {
+	private Random rand = new Random();
+
 	protected RelayMK1() {
 		super(Material.iron);
 		this.setBlockName(ModInfo.MOD_ID + ".relayMK1");
@@ -110,6 +116,43 @@ public class RelayMK1 extends BlockContainer {
 			world.setBlockMetadataWithNotify(x, y, z, 4, 2);
 		}
 
+	}
+
+	@Override
+	public void breakBlock(World world, int x, int y, int z, Block block, int oldMetadata) {
+
+		TileRelayMK1 tileentity = (TileRelayMK1) world.getTileEntity(x, y, z);
+
+		if (tileentity != null) {
+			for (int i = 0; i < tileentity.getSizeInventory(); i++) {
+				ItemStack itemstack = tileentity.getStackInSlot(i);
+
+				if (itemstack != null) {
+					float f = this.rand.nextFloat() * 0.8F + 0.1F;
+					float f1 = this.rand.nextFloat() * 0.8F + 0.1F;
+					float f2 = this.rand.nextFloat() * 0.8F + 0.1F;
+
+					while (itemstack.stackSize > 0) {
+						int j = this.rand.nextInt(21) + 10;
+
+						if (j > itemstack.stackSize) {
+							j = itemstack.stackSize;
+						}
+
+						itemstack.stackSize -= j;
+
+						EntityItem item = new EntityItem(world, (double) ((float) x + f), (double) ((float) y + f1), (double) ((float) z + f2), new ItemStack(itemstack.getItem(), j, itemstack.getItemDamage()));
+						System.out.println("Item is "+ itemstack.getDisplayName());
+						if (itemstack.hasTagCompound()) {
+							item.getEntityItem().setTagCompound((NBTTagCompound) itemstack.getTagCompound().copy());
+						}
+
+						float f4;
+					}
+
+				}
+			}
+		}
 	}
 
 	@Override
--- src/main/java/projecte/container/ContainerEnergyCollectorMK1.java
+++ src/main/java/projecte/container/ContainerEnergyCollectorMK1.java
@@ -4,26 +4,24 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import projecte.tile.TileEnergyCollectorMK1;
 
 public class ContainerEnergyCollectorMK1 extends Container {
 
-	//private TileEnergyCollectorMK1 tile;
+	// private TileEnergyCollectorMK1 tile;
 
-	public ContainerEnergyCollectorMK1(InventoryPlayer inventory,
-			TileEnergyCollectorMK1 tileentity) {
-		//this.tile = tileentity;
+	public ContainerEnergyCollectorMK1(InventoryPlayer inventory, TileEnergyCollectorMK1 tileentity) {
+		// this.tile = tileentity;
 
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 2; j++) {
-				this.addSlotToContainer(new SlotEmcFuel(tileentity, j + i * 2,
-						20 + j * 18, 8 + i * 18));
+				this.addSlotToContainer(new SlotEmcFuel(tileentity, j + i * 2, 20 + j * 18, 8 + i * 18));
 			}
 		}
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 9; j++) {
-				this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9,
-						8 + j * 18, 84 + i * 18));
+				this.addSlotToContainer(new Slot(inventory, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
 			}
 		}
 
@@ -37,10 +35,10 @@ public class ContainerEnergyCollectorMK1 extends Container {
 		return true;
 	}
 
-	// @Override
-	// public ItemStack transferStackInSlot(EntityPlayer player, int i){
-	// return null;
-	//
-	// }
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int i) {
+		return null;
+
+	}
 
 }
