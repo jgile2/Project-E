2b1c085febceb9f31267497717b33fee48d109eb
jgile2 <jgile2@optusnet.com.au>
2014-04-27 01:17:00 +1000

    some stuff

---
+++ src/main/java/projecte/container/slot/SlotBag.java
@@ -0,0 +1,22 @@
+package projecte.container.slot;
+
+import projecte.items.PEItems;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+
+public class SlotBag extends Slot{
+	int currentSlot=0;
+	public SlotBag(int slot,IInventory par1iInventory, int id, int x, int y) {
+		super(par1iInventory, id, x, y);
+		currentSlot = slot; 
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public boolean canTakeStack(EntityPlayer player) {
+		if(player.inventory.getStackInSlot(currentSlot).getItem()==PEItems.AlchemyBag)return false;
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/projecte/packet/PacketBase.java
@@ -0,0 +1,22 @@
+package projecte.packet;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.world.World;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public abstract class PacketBase {
+	  public abstract void encode(ByteArrayDataOutput output);
+
+	    public abstract void decode(ByteArrayDataInput input);
+
+	    @SideOnly(Side.CLIENT)
+	    public abstract void actionClient(World world, EntityPlayer player);
+
+	    public abstract void actionServer(World world, EntityPlayerMP player);
+}
